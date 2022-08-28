d4e52be728a2f87e1be07c0189e346d36753e533
NewUsernameMadnessZmey <madnesszmey@gmail.com>
2022-08-28 19:03:52 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,113 @@
+#include "iostream"
+
+using namespace std;
+
+class Matrix
+{
+private:
+	double array[3][3];
+
+public:
+
+	Matrix()
+	{
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				array[i][j] = 0;
+		cout << "Вызов конструктора Matrix" << endl;
+
+	}
+	/*~Matrix()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			delete[] array[j];
+		}
+		delete[] array;
+	}*/
+
+
+	void Print()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				cout << array[i][j];
+				cout << "\t";
+			}
+			cout << endl;
+		}
+	}
+
+
+	Matrix(const Matrix& a)						//Конструктор копирования
+	{
+
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				array[i][j] = a.array[i][j];
+		cout << "Вызов Конструктор копирования Matrix" << endl;
+	}
+
+	Matrix SetRandom()
+	{
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				this->array[i][j] = rand() % 10;
+		cout << "Установка рандомных значений Matrix" << endl;
+		return *this;
+
+	}
+
+
+
+	Matrix& operator=(const Matrix& CopyMatrix)			//Оператор копирования
+	{
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				array[i][j] = CopyMatrix.array[i][j];
+		return *this;
+		cout << "Вызов Оператор копирования Matrix" << endl;
+	}
+};
+
+
+
+
+
+
+
+void main()
+{
+
+	setlocale(LC_ALL, "ru");
+
+	Matrix Proba;
+
+	Proba.Print();
+	cout << endl;
+
+	Proba.SetRandom();
+
+	Proba.Print();
+	cout << endl;
+
+	Matrix Copy(Proba);
+	Copy.Print();
+	cout << endl;
+
+	Proba.SetRandom();
+	Proba.Print();
+	cout << endl;
+
+	Copy = Proba;
+	Copy.Print();
+	cout << endl;
+
+
+	system("pause");
+}
+
+
